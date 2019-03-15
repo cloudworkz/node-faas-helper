5f85b229d0be8fa688ed7916f330409aa7344b24
Chris Froehlingsdorf <chris@5cf.de>
2019-03-15 15:06:00 +0100

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# gcf-helper CHANGELOG
+
+## 2019-03-15, Version 0.1.0
+
+* Initial release with changelog
---
+++ index.ts
@@ -0,0 +1,3 @@
+import GCFHelper from "./lib/GCFHelper";
+export default GCFHelper;
+export { GCFHelper }; // faked object export
