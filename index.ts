7f86637f9caabf86e262809cdf06954e6bc4f7b7
yacut <roman.rogozhnikov@gmail.com>
2019-03-28 16:19:50 +0100

    add kms and sql

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ import GCFHelper from "./lib/GCFHelper";
 import { FunctionOptions } from "./lib/interfaces/FunctionOptions";
 export default GCFHelper;
 export { GCFHelper }; // faked object export
-export const setup = ((options: FunctionOptions) => new GCFHelper(options));
+export const setup = (options?: FunctionOptions) => new GCFHelper(options);
